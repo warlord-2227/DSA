7249626a519e7d9c85e721f8373ce4993e972811
warlord-2227 <setu.iitb@gmail.com>
2023-07-20 15:51:41 +0530

    added edge condition for binary search

--- searching/binary_search.py
+++ searching/binary_search.py
@@ -6,20 +6,24 @@ import sys
 
 #a = [2,3,4,10,40]
 a=[1,4,5,6,10,12,54,68,71,100]
+#a =[1]
 #write an algorithm to get element from an sorted array in optimized way 
 def binary_search(a,start,end,anchor):
-    mid = start + ((end-start)//2)
-    #print(mid,start,end)
-    if a[mid]==anchor:
-        return mid 
-    elif a[mid]<anchor:
-       return  binary_search(a,mid,end,anchor)
-    elif a[mid]>anchor:
-       return  binary_search(a,start,mid-1,anchor)
+    if end>=start:
+        mid = start + ((end-start)//2)
+        #print(mid,start,end)
+        if a[mid]==anchor:
+            return mid 
+        elif a[mid]<anchor:
+           return  binary_search(a,mid+1,end,anchor)
+        else:
+           return  binary_search(a,start,mid-1,anchor)
     #If no solution found then exit with -1
-    return -1
+    else:
+        return -1
 
 #anchor = 10
 anchor = 71
+#anchor = 10
 #print(len(a),a)
 print(binary_search(a,0,len(a)-1,anchor))
